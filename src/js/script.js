34c5bf0c6ac937419eb3d6001b21e284520b8ae7
Artem <example@gmail.com>
2024-09-20 12:23:10 +0300

    new message

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+
+
+
+// const result = confirm("ты лох?");
+// console.log(result);
+
+// const answer = prompt ("вам есть 18?", "18");
+// console.log(answer);
+
+const answer = [];
+
+answer[0] = prompt('как ваше имя', '');
+answer[1] = prompt('как ваше фамилия', '');
+answer[2] = prompt('как ваше призвище', '');
+document.write(answer);
